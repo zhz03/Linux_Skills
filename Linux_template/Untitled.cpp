ce31fd6db56bdd048532338d5d64a7a96f4c502d
Zhaoliang <zhz03@g.ucla.edu>
2023-11-04 13:46:59 -0700

    add untitled files as template

---
+++ Linux_template/Untitled.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2023 by UCLA. All rights reserved.
+ * 
+ * Author: Zhaoliang Zheng <zhz03@g.ucla.edu>
+ * License: TDG-Attribution-NonCommercial-NoDistrib
+ */
+
+int main() {
+  return 0;
+}
---
+++ Linux_template/Untitled.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Code description.
+"""
+# Author: Zhaoliang Zheng <zhz03@g.ucla.edu>
+# License: TDG-Attribution-NonCommercial-NoDistrib
+
+if __name__ == "__main__":
+    pass
